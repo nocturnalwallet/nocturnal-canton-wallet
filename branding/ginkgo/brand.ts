ec907cacd6289b5d745f19723f79d60e7c54e9a8
Le Hoang Anh <anh.le@angelhack.com>
2026-08-19 10:50:41 +0700

    chore(release): bump Ginkgo to 0.6.1 and Nocturnal to 0.2.1

--- branding/ginkgo/brand.ts
+++ branding/ginkgo/brand.ts
@@ -10,7 +10,7 @@ const brand: BrandConfig = {
   providerName: 'Ginkgo',
   partyHintDefault: 'ginkgo-wallet',
   logTag: '[Ginkgo]',
-  version: '0.6.0',
+  version: '0.6.1',
   manifestKey:
     'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA1wm7Jt0cFnGf9ecUzFcSNx2NjY6ayMNQw8d4xgjW41L5ue7FRcODaFWngjxsdgiomU01LMgGHRD5eLbM7mi/iqKs0jYKooKRQk5TaDnZyAHtluDTzeCLBa+QXBJbx3qC88vWRSoDkmEIq6EQ0KlAk3o120IXRY6UYdn6TXXvqLo4vhWya8WfBTLorQtJJo7ByghgIFXDkSYiAvaSeiPAf2bxGU8l+HXJHIYouKECJHPoBW3CB626HXUWVeFeysEl4i/JNgdL5TAmb8KmaSly2T0q2KG7vXn+Dax2yUwGv2Y9X30Nw+8BZAAXd8N2goETWtXeZtgLZ5lxHCS6kkgksQIDAQAB',
   hostPermissions: [],
